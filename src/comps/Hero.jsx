c07a04ac9acb4668810c62ed68ed953ca5e7cf66
Paul Abib Camano <paul.camano@elinnovtech.com>
2023-06-26 23:34:39 +0800

    Hero: fix vertical padding on 2xl screens

--- src/comps/Hero.jsx
+++ src/comps/Hero.jsx
@@ -1,6 +1,6 @@
 export default function Hero({ siteName, jobTitle }){
     return(
-        <div className="py-16 lg:py-32 px-4 lg:mx-0 rounded shadow-2xl lg:mt-24">
+        <div className="py-16 lg:py-32 2xl:py-48 px-4 lg:mx-0 rounded shadow-2xl lg:mt-24">
             <h1 className="text-2xl text-center sm:text-start md:text-4xl lg:text-7xl antialiased font-mono leading-relaxed">Hi! I'm <span className="decoration-1 bg-blue-950 hover:bg-white hover:text-blue-950 rounded p-3">{siteName}</span>!</h1>
             <h2 className="text-xl text-center sm:text-start md:text-2xl lg:text-4xl mt-2">{jobTitle}</h2>
             <a className="block shadow-lg lg:mx-0 md:text-xl mx-auto sm:mx-0 py-3 md:py-6 mt-16 bg-slate-900 hover:bg-slate-800 w-48 text-center rounded" href="#Contact">Contact Me</a>
