8cbbcba9d82e5120fde0611ca650439a9e2a2f36
Paul Abib Camano <paul.camano@elinnovtech.com>
2023-06-26 23:24:55 +0800

    Dealt with responsive web design

--- src/comps/Header.jsx
+++ src/comps/Header.jsx
@@ -3,7 +3,7 @@ export default function Header({ siteName, links }){
         <header className="flex justify-between py-3 px-4 sticky top-0 bg-gradient-to-r from-indigo-900 to-black">
             <button onClick={() => {  window.scrollTo({ top: 0, left: 0, behavior: "smooth" }) }} className="text-3xl font-semibold tracking-widest hover:text-blue-500 hover:cursor-pointer">{siteName}</button>
             <nav className="my-auto">
-                <ul className="text-xl flex gap-3 lg:visible">
+                <ul className="text-xl gap-3 hidden sm:flex">
                     {links && links.map((link, key) => {
                         const {text, href} = link
                         return <li className="hover:text-blue-500 hover:underline decoration-dashed" key={key}><a href={href}>{text}</a></li>
