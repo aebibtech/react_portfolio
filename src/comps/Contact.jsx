6d6bec23aafd66f6fe978abfcaf2a4d45a1994b1
Paul Abib Camano <paul.camano@elinnovtech.com>
2023-06-27 09:31:53 +0800

    Destructure contact object on Contact component

--- src/comps/Contact.jsx
+++ src/comps/Contact.jsx
@@ -4,18 +4,18 @@ function ContactButton({ link, icon }){
 }
 
 export default function Contact({ contact }){
-    console.log(contact)
+    const { github, gitlab, linkedin, youtube, facebook, messenger, twitter } = contact
     return(
         <div className="p-4 mt-4 sm:mt-48" id="Contact">
             <h3 className="text-3xl sm:text-4xl md:text-7xl text-center mb-12">Where to find me</h3>
             <div className="flex justify-center gap-4">
-                <ContactButton link={contact.github} icon="/assets/img/github.png" />
-                <ContactButton link={contact.gitlab} icon="/assets/img/github.png" />
-                <ContactButton link={contact.linkedin} icon="/assets/img/linkedin.png" />
-                <ContactButton link={contact.youtube} icon="/assets/img/youtube.png" />
-                <ContactButton link={contact.facebook} icon="/assets/img/facebook.png" />
-                <ContactButton link={contact.messenger} icon="/assets/img/messenger.png" />
-                <ContactButton link={contact.twitter} icon="/assets/img/twitter.png" />
+                <ContactButton link={github} icon="/assets/img/github.png" />
+                <ContactButton link={gitlab} icon="/assets/img/github.png" />
+                <ContactButton link={linkedin} icon="/assets/img/linkedin.png" />
+                <ContactButton link={youtube} icon="/assets/img/youtube.png" />
+                <ContactButton link={facebook} icon="/assets/img/facebook.png" />
+                <ContactButton link={messenger} icon="/assets/img/messenger.png" />
+                <ContactButton link={twitter} icon="/assets/img/twitter.png" />
             </div>
         </div>
     )
