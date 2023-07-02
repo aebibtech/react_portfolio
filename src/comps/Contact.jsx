e45f25ac9d48de39bbee6d6061b2025ed42d9cc2
Paul Abib Camano <paul.camano@elinnovtech.com>
2023-07-02 23:49:33 +0800

    Implement DRY on Contact component

--- src/comps/Contact.jsx
+++ src/comps/Contact.jsx
@@ -13,13 +13,9 @@ export default function Contact(){
         <div className="p-4 mt-4 sm:mt-48" id="Contact">
             <h3 className="text-3xl sm:text-4xl md:text-7xl text-center mb-12">Where to find me</h3>
             <div className="flex justify-center gap-4">
-                {conf.contact && conf.contact.github && <ContactButton link={conf.contact.github} icon="/assets/img/github.png" />}
-                {conf.contact && conf.contact.gitlab && <ContactButton link={conf.contact.gitlab} icon="/assets/img/github.png" />}
-                {conf.contact && conf.contact.linkedin && <ContactButton link={conf.contact.linkedin} icon="/assets/img/linkedin.png" />}
-                {conf.contact && conf.contact.youtube && <ContactButton link={conf.contact.youtube} icon="/assets/img/youtube.png" />}
-                {conf.contact && conf.contact.facebook && <ContactButton link={conf.contact.facebook} icon="/assets/img/facebook.png" />}
-                {conf.contact && conf.contact.messenger && <ContactButton link={conf.contact.messenger} icon="/assets/img/messenger.png" />}
-                {conf.contact && conf.contact.twitter && <ContactButton link={conf.contact.twitter} icon="/assets/img/twitter.png" />}
+                {conf.contact && Object.keys(conf.contact).map(socmed => {
+                    return conf.contact[socmed] && <ContactButton link={conf.contact[socmed]} icon={`/assets/img/${socmed}.png`} />
+                })}
             </div>
         </div>
     )
