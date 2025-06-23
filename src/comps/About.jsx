7e1a2974b78fa9546a58543203b872ce28d4834f
Paul Abib S. Camano <aebibtech@gmail.com>
2025-06-23 17:51:12 +0000

    content updates, layout updates

--- src/comps/About.jsx
+++ src/comps/About.jsx
@@ -7,9 +7,9 @@ export default function About(){
     return(
         <div className="py-16 px-4 mt-4 sm:mt-48 shadow-2xl" id="About">
             <h3 className="text-3xl sm:text-4xl md:text-7xl text-center mb-12">About Me</h3>
-            <div className="flex justify-center items-center gap-4">
-                <figure>
-                    <img className="rounded-full" src={conf.about && conf.about.pic_link} alt="profile_pic" />
+            <div className="flex flex-col sm:flex-row justify-center items-center gap-4 max-w-4xl mx-auto">
+                <figure className="w-48 h-48 flex-shrink-0 align-center justify-center rounded-full overflow-hidden shadow-lg">
+                    <img src={conf.about && conf.about.pic_link} alt="profile_pic" />
                 </figure>
                 <div>
                     <p className="text-3xl">{conf.about && conf.about.name}</p>
--- src/comps/Hero.jsx
+++ src/comps/Hero.jsx
@@ -5,9 +5,9 @@ export default function Hero(){
     const { conf } = useContext(GlobalContext)
     
     return(
-        <div className="h-screen py-16 lg:py-32 2xl:py-48 px-4 lg:mx-0 rounded shadow-2xl flex flex-col justify-center items-center">
+        <div className="h-screen rounded shadow-2xl flex flex-col justify-center items-center">
             <h1 className="text-2xl md:text-4xl lg:text-7xl antialiased font-mono leading-relaxed">Hi! I'm <span className={`decoration-1 ${HeroHoverThemes[conf.theme]} rounded p-3`}>{conf.site_name}</span>!</h1>
-            <h2 className="text-xl mt-4 md:text-2xl lg:text-4xl">{conf.job_title}</h2>
+            <h2 className="text-xl mt-4 md:text-2xl lg:text-4xl text-center ">{conf.job_title}</h2>
             <a className={`inline-block font-medium shadow-lg md:text-xl py-3 md:py-4 mt-16 ${HeroButtonThemes[conf.theme]} hover:opacity-75 w-48 text-center rounded`} href="#Contact">Contact Me</a>
         </div>
     )
